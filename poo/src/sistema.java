dc82353ce8fbbf2438119eb192ac6d87e70b4d9d
neguu <32901676+neguu@users.noreply.github.com>
2017-11-28 08:30:19 -0300

    Update sistema.java

--- poo/src/sistema.java
+++ poo/src/sistema.java
@@ -7,3 +7,28 @@ public class sistema{
         
      }
 }
+public String cadastrar() {
+		Scanner l = new Scanner(System.in);
+		System.out.println("BEM VINDO AO CADASTRO DE USUÁRIOS");
+		System.out.println("Digite os dados: " + "\nNome ");
+		String nome = l.next();
+		System.out.println("Login desejado ");
+		String login = l.next();
+		System.out.println("idade");
+		int idade = l.nextInt();
+		System.out.println("Senha de acesso");
+		String senha = l.next();
+		
+		Usuario usuario = new Usuario();
+		
+		
+		usuario.setNome(nome);
+		usuario.setLogin(login);
+;		usuario.setIdade(idade);
+		usuario.setSenha(senha);
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(usuario);
+		System.out.println(usuario.getNome() + "\n" + usuario.getLogin() + "\n" + usuario.getIdade());
+		
+		return usuario.toString();
+	}
